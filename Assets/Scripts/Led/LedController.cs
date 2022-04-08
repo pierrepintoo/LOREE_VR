ccd271a5fe9f07922f2b13d2267543a67cf83b15
Axel Risser <axel.risser94@gmail.com>
2022-04-08 13:02:52 +0200

    ✨ Add audio visualization to the led

--- Assets/Scripts/Led/LedController.cs
+++ Assets/Scripts/Led/LedController.cs
@@ -4,29 +4,88 @@ using UnityEngine;
 
 public class LedController : MonoBehaviour
 {
-    [SerializeField] AudioSource audio;
+    [SerializeField] AudioSource audioSource;
     [SerializeField] Material ledMaterial;
+    [SerializeField] float fillMultiplier;
+    float[] samples = new float[512];
+    float[] freqBand = new float[8];
+    public float[] bandBuffer = new float[8];
+    float[] bufferDecrease = new float[8];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float[] spectrum = new float[256];
-        audio.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
-        float sum = 0.0f;
-        for (int i = 1; i < spectrum.Length - 1; i++)
+        // ======== OLD CODE ========
+        // float[] spectrum = new float[256];
+        // audio.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+        // float sum = 0.0f;
+        // for (int i = 1; i < spectrum.Length - 1; i++)
+        // {
+        //     sum += spectrum[i];
+        // }
+        // float moyenne = sum / spectrum.Length;
+        // float heightLeds = moyenne * 100;
+        // ledMaterial.SetFloat("_Fill", heightLeds);
+        // Debug.Log(heightLeds);
+        // ==========================
+
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+        ledMaterial.SetFloat("_Fill", bandBuffer[0] * fillMultiplier);
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void BandBuffer()
+    {
+        for (int g = 0; g < 8; g++)
         {
-            sum += spectrum[i];
+            if (freqBand[g] > bandBuffer[g])
+            {
+                bandBuffer[g] = freqBand[g];
+                bufferDecrease[g] = 0.005f;
+            }
+
+            if (freqBand[g] < bandBuffer[g])
+            {
+                bandBuffer[g] -= bufferDecrease[g];
+                bufferDecrease[g] *= 1.2f;
+            }
         }
-        float moyenne = sum / spectrum.Length;
-        float heightLeds = moyenne * 100;
-        // float volume = audio.getVolume();
-        // ledMaterial.SetFloat("_Fill", Mathf.Lerp(0.0f, 1.0f, heightLeds));
-        ledMaterial.SetFloat("_Fill", heightLeds);
-        Debug.Log(heightLeds);
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if(i == 7) {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            freqBand[i] = average * 10;
+        }
     }
 }
